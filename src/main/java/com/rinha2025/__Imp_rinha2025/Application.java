ad77aea54617f695b81da38e26a7c8dde7eca6f5
Gabriel Vinícius <gabrielvto18@gmail.com>
2025-07-26 20:47:17 -0300

    Organizando a casa

---
+++ src/main/java/com/rinha2025/__Imp_rinha2025/Application.java
@@ -0,0 +1,13 @@
+package com.rinha2025.__Imp_rinha2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinha2025/__Imp_rinha2025/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinha2025.__Imp_rinha2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
